2b24354620b4931835df0224a88a0b0abc8603b5
name <varunbrahmane30@gmail.com>
2021-07-06 18:04:02 +0530

    [varun] added: functional programs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace D5BasicPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Basic core C# progrms");
+            FlipCoin.checkFlipCoin();
+        }
+    }
+}
