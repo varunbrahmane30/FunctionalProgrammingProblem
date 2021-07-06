6b2ef0abf399c158d26b7c12e55d2574de0a8308
name <varunbrahmane30@gmail.com>
2021-07-06 18:05:33 +0530

    [varun] added: flip coin program

---
+++ FlipCoin.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D5BasicPrograms
+{
+    class FlipCoin
+    {
+        public static void checkFlipCoin()
+        {
+            // generating random 
+            Random random =new Random();
+            int value = random.Next(0,2);
+
+            //variables declaration
+            int percentageTail = 0;
+            int percentageHead = 0;
+
+            
+            // implementation 
+            if( value < 0.5)
+            {
+               // Console.WriteLine("Tails");
+                percentageTail++;
+            }
+            else
+            {
+               // Console.WriteLine("Heads");
+                percentageHead++;
+            }
+
+            // result display on console 
+            Console.WriteLine("Percentage of head is : " + percentageHead);
+            Console.WriteLine("Percentage of Tail is : " + percentageTail);
+
+
+
+        }
+    }
+}
